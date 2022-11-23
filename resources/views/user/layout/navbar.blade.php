af31fe33f4e6a4a993b61beb6cf336b9bd8e7f16
msyiham <muhammad.syiham.2105336@students.um.ac.id>
2022-11-23 22:51:48 +0700

    mbenakno halaman customer

--- resources/views/user/layout/navbar.blade.php
+++ resources/views/user/layout/navbar.blade.php
@@ -1,11 +1,3 @@
-<style>
-  .nav-item.active {
-  color: rgb(219, 212, 22);
-}
-.nav-link.active {
-  color: rgb(219, 212, 22);
-}
-</style>
 <div class="sub_page">
     <div class="hero_area">
         <div class="bg-box">
@@ -24,22 +16,22 @@
     
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav  mx-auto ">
-                  <li class="nav-item">
-                    <a class="nav-link @yield('home')" href="/home">Beranda<span class="sr-only">(current)</span></a>
+                  <li class="nav-item @if(request()->is('/home')) active @endif">
+                    <a class="nav-link @if(request()->is('/home')) active @endif" href="/home">Beranda<span class="sr-only">(current)</span></a>
                   </li>
-                  <li class="nav-item ">
+                  <li class="nav-item">
                     <a class="nav-link @if(request()->is('/shop')) active @endif" href="/shop">Belanja </a>
                   </li>
-                  <li class="nav-item @if(request()->is('TUBES/about')) active @endif">
+                  <li class="nav-item @if(request()->is('/about')) active @endif">
                     <a class="nav-link" href="/about">Tentang Kami</a>
                   </li>
                 </ul>
                 <div class="user_option">
                   <a href="/profil" class="user_link">
-                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <i class="fa fa-user" aria-hidden="true" title="Profil"></i>
                   </a>
-                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
-                    </svg>
+                  <a href="/logout" class="user_link">
+                    <i class="fa fa-sign-out" data-toggle="tooltip" title="Logout" style="padding: 8px"></i>
                   </a>
                 </div>
               </div>
